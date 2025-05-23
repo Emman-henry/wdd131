7c0abb4e0b3b80c249223cd46db5504b4e0a778b
emman-henry <officialgr8henry@gmail.com>
2025-05-23 23:12:47 +0100

    new project

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Set last modified date of the document
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
